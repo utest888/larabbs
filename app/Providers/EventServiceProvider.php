4aa9f6dfa49c9b5c100a0b6fb9839e76db2b71ef
unknown <utest888@qq.com>
2020-11-21 18:22:21 +0800

    '找回密码成功消息提示'

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        \Illumiate\Auth\Events\Verified::class => [
+            \App\Listeners\EmailVerified::class
+        ]
     ];
 
     /**
