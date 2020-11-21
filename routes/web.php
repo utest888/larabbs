207f915c0711af26525e61e75c8eae668f90da66
unknown <utest888@qq.com>
2020-11-21 17:22:05 +0800

    '增加验证码'

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PagesController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PagesController::class, 'root'])->name('root');
+
+Auth::routes();
