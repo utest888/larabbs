6c757f8e89e1f4837c8e9d5724e9096bc822fc81
unknown <utest888@qq.com>
2020-11-22 14:00:17 +0800

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -1,8 +1,26 @@
 <?php
 
+use App\Http\Requests\Request;
 use Illuminate\Support\Facades\Route;
 
 function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
+}
+
+function is_topic_index()
+{
+    if (Route::currentRouteName() == 'topics.index') {
+        return 'active';
+    }
+
+    return '';
+}
+
+function category_nav_active($category_id)
+{
+    if (Route::currentRouteName() == 'categories.show' &&  request('category')->id == $category_id) {
+        return 'active';
+    }
+    return '';
 }
