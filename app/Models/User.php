a674866c7694dea609ad57ef37191a511fb0d2fc
unknown <utest888@qq.com>
2020-11-22 14:32:26 +0800

    用户话题列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,11 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable, MustVerifyEmailTrait;
 
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
