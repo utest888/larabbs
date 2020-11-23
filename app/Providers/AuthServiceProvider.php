ee3a58a7d4a71e42faa72e0d48ce8e2d6489c56c
unknown <utest888@qq.com>
2020-11-23 18:30:25 +0800

    增加切换用户插件

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\Horizon;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-		 \App\Models\Reply::class => \App\Policies\ReplyPolicy::class,
+        \App\Models\Reply::class => \App\Policies\ReplyPolicy::class,
         \App\Models\Topic::class => \App\Policies\TopicPolicy::class,
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
@@ -26,6 +28,10 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+
+        Horizon::auth(function ($request) {
+            return Auth::user()->hasRole('Founder');
+        });
 
 
 
