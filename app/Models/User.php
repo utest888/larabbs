d9de8c255023dc5dd90b3721b76dd23bd9a2f038
unknown <utest888@qq.com>
2020-11-23 19:39:00 +0800

    用户

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,12 +9,31 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     use HasFactory, MustVerifyEmailTrait, HasRoles;
     use Notifiable {
         notify as protected laravelNotify;
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        if (strlen($value) != 60) {
+            $value = bcrypt($value);
+        }
+        $this->attributes['password'] = $value;
+    }
+
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
+        if (!Str::startsWith($path, 'http')) {
+            // 拼接完整的 URL
+            $path = config('app.url') . "/uploads/images/avatars/$path";
+        }
+        $this->attributes['avatar'] = $path;
     }
 
     public function markAsRead()
