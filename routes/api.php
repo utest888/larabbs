a15d6ad52ef99d5f9adcf65e04982eb64adc6130
unknown <utest888@qq.com>
2020-11-24 22:32:32 +0800

    categories index

--- routes/api.php
+++ routes/api.php
@@ -46,6 +46,8 @@ $api->version('v1', [
         $api->put('authorizations/current', 'AuthorizationsController@update')->name('api.authorizations.update');
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')->name('api.authorizations.destroy');
 
+        $api->get('categories', 'CategoriesController@index')->name('api.categories.index');
+
         //需要token验证的接口
         $api->group(['middleware' => 'api.auth'], function ($api) {
             //当前登录用户信息
