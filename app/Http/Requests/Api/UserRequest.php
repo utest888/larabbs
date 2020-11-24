df35b33da0b103b61ccca727fe770a441da77966
unknown <utest888@qq.com>
2020-11-24 21:33:08 +0800

    添加用户

--- app/Http/Requests/Api/UserRequest.php
+++ app/Http/Requests/Api/UserRequest.php
@@ -56,4 +56,14 @@ class UserRequest extends FormRequest
             'introduction' => '个人简介'
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'name.unique' => '用户名已被占用，请重新填写',
+            'name.regex' => '用户名只支持英文数字',
+            'name.between' => '用户名必需介于3-25个字符之间',
+            'name.required' => '用户名不能为空',
+        ];
+    }
 }
